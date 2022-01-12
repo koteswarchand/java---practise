f5a1780d38522d21ca0f612703548c57a51261a4
koteswarchand <69597390+koteswarchand@users.noreply.github.com>
2022-01-12 23:55:05 +0530

    Creating variables in shell scripting

---
+++ variables.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#variables
+
+echo "variables"
+#no space shoulld be give before = or after = while assigning
+name="peter"
+echo "$name"
+
+a="Koteswarchand"
+echo "$a"
+
+b=1234
+echo "$b"
+
+c='an'
+echo "$c"
+
+d="$a"
+echo "$d"
+
+e="hello"
+echo "$e"
+
+f=123
+echo "$f"
+
+h='abcsd'
+echo "$h"
